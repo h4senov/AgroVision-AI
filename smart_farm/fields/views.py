3e37a92672af30291895231e80d3910b68a61517
h4senov <hesen0202@mail.ru>
2026-03-04 13:36:05 +0400

    fix fields/view

--- smart_farm/fields/views.py
+++ smart_farm/fields/views.py
@@ -1,4 +1,4 @@
-from pyexpat.errors import messages
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from sensors.models import Sensor
