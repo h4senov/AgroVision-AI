cc43acc80cbe866214a3c512623f6ae3e076e13e
h4senov <hesen0202@mail.ru>
2026-03-07 21:58:03 +0400

    update v1.2.3 / fix SMTP error

--- smart_farm/smart_farm/settings.py
+++ smart_farm/smart_farm/settings.py
@@ -177,17 +177,14 @@ LOGIN_URL = '/users/login/'
 
 
 
+import os
+from dotenv import load_dotenv
+load_dotenv()  # lokal üçün işləyir
+
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
- 
-DEFAULT_FROM_EMAIL  = 'AgroVision AI <agrovision.ai.az@gmail.com>'
-
-
- 
-from dotenv import load_dotenv
-load_dotenv()
-
-EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL  = f'AgroVision AI <{EMAIL_HOST_USER}>'
